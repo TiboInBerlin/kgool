4860f62ff0b50c637d12c08357ca6619048a98d4
Thibaut Valarche <thibautvalarche@gmail.com>
2018-08-12 17:19:13 +0200

    all pages structured

--- server.js
+++ server.js
@@ -27,11 +27,19 @@ app.use(
 
 app.set("view engine", "handlebars");
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
+        req.session.loggedIn != true;
+    res.render("welcome"); // when the user type "/register", user will be redirected to the register view
+});
+
+app.get("/welcome", (req, res) => {
     if (req.session.loggedIn != true) {
-        res.redirect("/login");
+        res.render("welcome", {
+            //remember: res.render is for template
+            layout: "main"
+        });
     } else {
-        res.render("home", {
+        res.render("welcome", {
             //remember: res.render is for template
             layout: "main"
         });
@@ -39,23 +47,132 @@ app.get("/home", (req, res) => {
     //db.getSigners()
 });
 
-
-app.get("/", (req, res) => {
-    req.session.loggedIn != true;
-    res.render("home"); // when the user type "/register", user will be redirected to the register view
-});
-
-
 app.get("/login", (req, res) => {
     //in the get, i ahve always to use a page! that s why: /login
     if (req.session.loggedIn != true) {
-        res.redirect("/login");
+        res.render("login");
     } else {
         res.render("login");
     }
 });
 
+app.get("/registerCustomer", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("registerCustomer");
+    } else {
+        res.render("registerCustomer");
+    }
+});
+
+app.get("/registerProducer", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("registerProducer");
+    } else {
+        res.render("registerProducer");
+    }
+});
+
+app.get("/profileProducer", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("profileProducer");
+    } else {
+        res.render("profileProducer");
+    }
+});
+
+app.get("/kundenInformation", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("kundenInformation");
+    } else {
+        res.render("kundenInformation");
+    }
+});
+
+app.get("/weitereInfo", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("weitereInfo");
+    } else {
+        res.render("weitereInfo");
+    }
+});
+
+app.get("/Favoriten", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("Favoriten");
+    } else {
+        res.render("Favoriten");
+    }
+});
+
+app.get("/presentationProducerSide", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("presentationProducerSide");
+    } else {
+        res.render("presentationProducerSide");
+    }
+});
+
+app.get("/profileCustomer", (req, res) => {
+    //in the get, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("profileCustomer");
+    } else {
+        res.render("profileCustomer");
+    }
+});
+
+app.get("/kundenInformationCustomer", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("kundenInformationCustomer");
+    } else {
+        res.render("kundenInformationCustomer");
+    }
+});
+
+app.get("/lieblingsfirmen", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("lieblingsfirmen");
+    } else {
+        res.render("lieblingsfirmen");
+    }
+});
+
+app.get("/contactProducerPage", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("contactProducerPage");
+    } else {
+        res.render("contactProducerPage");
+    }
+});
+
+app.get("/presentationProducerSide", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("presentationProducerSide");
+    } else {
+        res.render("presentationProducerSide");
+    }
+});
+
+app.get("/presentationCustomerSide", (req, res) => {
+    //in the sget, i ahve always to use a page! that s why: /login
+    if (req.session.loggedIn != true) {
+        res.render("presentationCustomerSide");
+    } else {
+        res.render("presentationCustomerSide");
+    }
+});
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log("listening to petition...");
+    console.log("Kgool!!!...");
 });
